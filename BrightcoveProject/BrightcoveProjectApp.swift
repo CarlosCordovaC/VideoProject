365ae7e1d6882bfef9e042cb78274b6bf6a3ed62
CarlosCordova <CarlosCordovaC@outlook.es>
2025-06-13 17:57:26 -0600

    Initial Commit

---
+++ BrightcoveProject/BrightcoveProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightcoveProjectApp.swift
+//  BrightcoveProject
+//
+//  Created by Carlos Camberos Cordova on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BrightcoveProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
